b08b4533ff1262bf7d78adb26a479ea60526ed6c
Wqimai <qiyan.wu@maimob.cn>
2017-06-06 18:41:31 +0800

    修改包名等

---
+++ app/src/main/java/com/maimob/weather/gson/City.java
@@ -0,0 +1,20 @@
+package com.maimob.weather.gson;
+
+/**
+ * Created by len_titude on 2017/5/14.
+ */
+
+public class City {
+
+    public Basic basic;
+
+    public String status;
+
+    public class Basic{
+        public String city;
+        public String cnty;
+        public String lat;
+        public String lon;
+        public String prov;
+    }
+}
